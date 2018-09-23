66bbc4a08ae8dadbb632ae2bf3e3b36ab3021ae2
anh le <lequocanh0010@gmail.com>
2018-09-23 09:06:10 +0700

    Seed data for api

---
+++ src/Services/Identity/WAL.Identity.API/Configuration/ApiConfig.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IdentityServer4;
+using IdentityServer4.Models;
+
+namespace WAL.Identity.API.Configuration
+{
+    public static class ApiConfig
+    {
+        // ApiResources define the apis in your system
+        public static IEnumerable<ApiResource> GetApis()
+        {
+            return new List<ApiResource>
+            {
+                new ApiResource("userlogs", "User Log Service"),
+            };
+        }
+
+        // Identity resources are data like user ID, name, or email address of a user
+        // see: http://docs.identityserver.io/en/release/configuration/resources.html
+        public static IEnumerable<IdentityResource> GetResources()
+        {
+            return new List<IdentityResource>
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile()
+            };
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>()
+            {
+                new Client
+                {
+                    ClientId = "client",
+
+                    // no interactive user, use the clientid/secret for authentication
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+
+                    // secret for authentication
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+
+                    // scopes that client has access to
+                    AllowedScopes = { "userlogs" }
+                }
+            };
+        }
+    }
+}
--- src/Services/Identity/WAL.Identity.API/Program.cs
+++ src/Services/Identity/WAL.Identity.API/Program.cs
@@ -1,14 +1,29 @@
-﻿namespace WAL.Identity.API
+﻿
+using IdentityServer4.EntityFramework.DbContexts;
+using Microsoft.EntityFrameworkCore;
+using WAL.Identity.API.Seeder;
+using WAL.ServiceHost.Extensions;
+
+namespace WAL.Identity.API
 {
+    using Autofac.Extensions.DependencyInjection;
     using Microsoft.AspNetCore;
     using Microsoft.AspNetCore.Hosting;
-    using Autofac.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Configuration;
 
     public class Program
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            BuildWebHost(args)
+                .MigrateDbContext<ConfigurationDbContext>((context, services) =>
+                {
+                    var configuration = services.GetService(typeof(IConfiguration)) as IConfiguration;
+
+                    new ConfigurationDbContextSeed()
+                        .SeedAsync(context, configuration)
+                        .Wait();
+                }).Run();
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
